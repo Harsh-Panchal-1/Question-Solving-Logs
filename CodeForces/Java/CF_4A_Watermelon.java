a5c8fdd2dd879cffcaa25766ef3f09b6af361150
Harsh-Panchal-1 <harsh.panchal1615@gmail.com>
2026-07-14 00:43:42 +0530

    Watermelon CF_4A Solved

---
+++ CodeForces/Java/CF_4A_Watermelon.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class CF_4A_Watermelon {
+  public static void main(String[] args){
+    java.util.Scanner sc = new Scanner(System.in);
+
+    int weight = sc.nextInt();
+    if ( weight % 2 == 0 && weight > 2) {
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+    sc.close();
+  }
+}
