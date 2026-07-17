4b1099204bb335f60e9add899cf087f4091e452f
Harsh-Panchal-1 <harsh.panchal1615@gmail.com>
2026-07-17 22:11:02 +0530

    CF_71A

---
+++ CodeForces/Python/CF_71A_WayTooLongWords.py
@@ -0,0 +1,11 @@
+wordCount = int(input())
+words = []
+for i in range(wordCount):
+  word = input()
+  words.append(word)
+for string in words:
+  length = len(string)
+  if length>10:
+    print(f"{string[0]}{(length-2)}{string[-1]}")
+  else:
+    print(string)
