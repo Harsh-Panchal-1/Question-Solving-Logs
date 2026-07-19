d67c747a01e55ddb4bf090b4d02394862a25ac28
Harsh-Panchal-1 <harsh.panchal1615@gmail.com>
2026-07-20 00:00:29 +0530

    CF_158A_Java

---
+++ CodeForces/Java/CF_231A_NextRound.java
@@ -0,0 +1,23 @@
+// Problem: 158A. Team
+// Link: https://codeforces.com/problemset/problem/158/A
+
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int k = scan.nextInt();
+    int nextRound = 0;
+    int[] playerScore = new int[n];
+    for (int i = 0; i < n; i++) {
+      int score = scan.nextInt();
+      playerScore[i] = score;
+    }
+    for (int i : playerScore) {
+      if (playerScore[k-1] <= i && i>0){
+        nextRound++;
+      }
+    }
+    System.out.println(nextRound);
+  }
+}
