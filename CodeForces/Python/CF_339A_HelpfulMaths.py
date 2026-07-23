d226f24326258f12a0233a11036a9a7c687fa711
Harsh-Panchal-1 <harsh.panchal1615@gmail.com>
2026-07-24 00:00:20 +0530

    CF_339A_Python

---
+++ CodeForces/Python/CF_339A_HelpfulMaths.py
@@ -0,0 +1,13 @@
+# Problem: 339A. Helpful Maths
+# Link: https://codeforces.com/problemset/problem/339/A
+
+s = input()
+
+numbers = s.split("+")
+
+numbers.sort()
+
+
+result = "+".join(numbers)
+
+print(result)
