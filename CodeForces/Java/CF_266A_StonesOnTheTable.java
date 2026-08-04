8b1bebad50556ecf38ab8268a0d67f177795dde7
Harsh-Panchal-1 <harsh.panchal1615@gmail.com>
2026-08-04 11:09:30 +0530

    CF_266A_Java

---
+++ CodeForces/Java/CF_266A_StonesOnTheTable.java
@@ -0,0 +1,19 @@
+//  Problem: 266A. Stones On the Table
+//  Link: https://codeforces.com/problemset/problem/266/A
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int stones = scan.nextInt();
+    String stoneString = scan.next();
+    int count =0;
+    for (int i = 0; i < (stoneString.length()-1); i++) {
+      if (stoneString.charAt(i) == stoneString.charAt(i+1)) {
+        count++;
+      }
+    }
+    System.out.println(count);
+  }
+}
