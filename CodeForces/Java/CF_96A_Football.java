64c59c0e87473f1109dfa2e2155e5e8be046a105
Harsh-Panchal-1 <harsh.panchal1615@gmail.com>
2026-08-16 15:50:11 +0530

    CF_96A_Python

--- CodeForces/Java/CF_96A_Football.java
+++ CodeForces/Java/CF_96A_Football.java
@@ -8,25 +8,11 @@ class Main {
     Scanner scan = new Scanner(System.in);
     String players = scan.next();
     scan.close();
-    
-    int count = 1; 
-    int prev = players.charAt(0);
 
-    for (int i = 1; i < players.length(); i++){
-      int cur = players.charAt(i);
-      
-      if (prev == cur){
-        count++;
-      } else {
-        count = 1; 
-        prev = cur;
-      }
-      
-      if (count >= 7){
-        System.out.println("YES");
-        return;
-      }
-    }
+    if (players.contains("1111111") || players.contains("0000000")) {
+    System.out.println("YES");
+    } else {
     System.out.println("NO");
+    }
   }
 }
---
+++ CodeForces/Python/CF_96A_Football.py
@@ -0,0 +1,9 @@
+# Problem: 96A. Football
+# Link: https://codeforces.com/problemset/problem/96/A
+
+players = input()
+
+if ("0000000" in players) or ("1111111" in players):
+  print("YES")
+else:
+  print("NO")
